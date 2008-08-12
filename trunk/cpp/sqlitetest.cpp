cb99d9045ea08a7c928eb4c142383eee54bd620a
mrwaltercool <mrwaltercool@96f54e04-8c51-0410-8021-111a85c9a89a>
2008-08-12 23:26:22 +0000

    I have added some python test code, using webpy and other methods with qt4 using qt4(cpp) and pyqt (python)
    More python code in this days ;)

--- trunk/cpp/sqlitetest.cpp
+++ trunk/cpp/sqlitetest.cpp
@@ -4,7 +4,26 @@
 #import <sqlite3.h>
 using namespace std;
 
-int main()
+bool databaseCreation()
 {
   
+}
+int main()
+{
+  sqlite3 *db;
+  char *zErrMsg = 0;
+  string rc;
+  rc = sqlite3_open("test.db", &db);
+  cout << rc;
+  rc = sqlite3_exec(db, "select * from data", NULL, 0, &zErrMsg);
+  //if(rc == 
+  //rc = sqlite3_exec(db, "insert into table2 (bla)values('poep')", NULL, 0, &zErrMsg);
+  cout << rc;
+  if( rc != SQLITE_OK )
+  {
+    sqlite3_free(zErrMsg);
+    databaseCreation;
+  }
+  sqlite3_close(db);
+  cin.get();
 }
