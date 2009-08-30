2d95560f09ac00444a8cf79847aead3b25b5488b
mrwaltercool <mrwaltercool@96f54e04-8c51-0410-8021-111a85c9a89a>
2009-08-30 03:10:10 +0000

    Added megadownload for python 3... recomended!
    - Fixed a bug on megadownload on some links with 0 bytes?

--- python/helloworld.py
+++ python/helloworld.py
@@ -1,10 +1,2 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-#
-# Developed by WalterCool! under GPL-2 License
-# Feel free of modify (using GPL-2 rules) or reporting bugs
-# mailto: waltercool [at] slash [dot] cl
-# http://www.slash.cl
-#
-
-print("asdf")
+print('Hello World')
