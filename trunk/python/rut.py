57498ebc43ab6f9a562d4d79051838d8503c4b5b
mrwaltercool <mrwaltercool@96f54e04-8c51-0410-8021-111a85c9a89a>
2009-09-02 02:56:48 +0000

    Rut: Fixed for problems when len < 8

--- trunk/python/rut.py
+++ trunk/python/rut.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 
 rut = input('Ingrese su rut: ')
+while len(rut) < 8: rut = '0' + rut
 temp = int(rut[0]) * 3 + int(rut[1]) * 2 + int(rut[2]) * 7 + int(rut[3]) * 6 + int(rut[4]) * 5 + int(rut[5]) * 4 + int(rut[6]) * 3 + int(rut[7]) * 2 
-temp = (temp%11 -11) * -1
-print(temp)
+print((temp%11 -11) * -1)
