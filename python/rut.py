150fcdfe328d1d81e866147d93d80d18408ea403
mrwaltercool <mrwaltercool@96f54e04-8c51-0410-8021-111a85c9a89a>
2009-09-02 03:06:01 +0000

    Rut: Final revision, now is modular

--- python/rut.py
+++ python/rut.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+#
+# Developed by WalterCool! under GPL-2 License
+# Feel free of modify (using GPL-2 rules) or reporting bugs
+# mailto: waltercool [at] slash [dot] cl
+# http://www.slash.cl
+#
+# This code is a bit bigger than 1st instance, for obvious reasons (ifs and function)
 
-rut = input('Ingrese su rut: ')
-while len(rut) < 8: rut = '0' + rut
-temp = int(rut[0]) * 3 + int(rut[1]) * 2 + int(rut[2]) * 7 + int(rut[3]) * 6 + int(rut[4]) * 5 + int(rut[5]) * 4 + int(rut[6]) * 3 + int(rut[7]) * 2 
-print((temp%11 -11) * -1)
+def rut(rut):
+  while len(rut) < 8: rut = '0' + rut
+  temp = int(rut[0]) * 3 + int(rut[1]) * 2 + int(rut[2]) * 7 + int(rut[3]) * 6 + int(rut[4]) * 5 + int(rut[5]) * 4 + int(rut[6]) * 3 + int(rut[7]) * 2
+  temp = (temp%11 -11) * -1
+  if temp == 10: return('K')
+  elif temp == 11: return('0')
+  else: return(temp)
+
+if __name__ == '__main__':
+  print( rut( input('Ingrese un rut: ') ) )
+
