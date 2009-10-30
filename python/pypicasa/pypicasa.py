c9aa959e8d09d373ee75b0043e11c00c64539d46
mrwaltercool <mrwaltercool@96f54e04-8c51-0410-8021-111a85c9a89a>
2009-10-30 20:12:58 +0000

    Pypicasa is now working

--- python/pypicasa/pypicasa.py
+++ python/pypicasa/pypicasa.py
@@ -9,34 +9,97 @@
 
 import gdata
 import gdata.photos.service
-import sys, getpass
+import sys, getpass, mimetypes, os
 
 class pypicasa():
   def __init__(self):
     self.gclient = gdata.photos.service.PhotosService()
   def login(self, email=None, passw=None):
     if email == None and passw == None:
-      self.gclient.email = raw_input("Username: ")
-      self.gclient.password = getpass.getpass(prompt="Password: ")
+      email = raw_input("Username: ")
+      passw = getpass.getpass(prompt="Password: ")
+    self.gclient.email = email
+    self.gclient.password = passw
     try:
       self.gclient.ProgrammaticLogin()
     except gdata.service.BadAuthentication:
       print("Bad login. Try again")
-      sys.exit(0)
+      self.login()
+      return True
     print("Now logged as " + self.gclient.email)
+    return True
   
-  def req_data(self):
-    self.albums = []
+  def req_data(self): #Add a list of albums
+    self.albums = {}
     info = self.gclient.GetUserFeed()
     for x in info.entry:
-      self.albums.append(x.title.text)
-    if self.albums.count("Pypicasa Uploaded") == 0:
-      print("Pypicasa album not detected...")
-      self.gclient.InsertAlbum("Pypicasa Uploaded","Uploaded pictures from pypicasa")
-      print("Pypicasa album created.")
-   def upload(self):
-     
+      self.albums[x.title.text] = x.gphoto_id.text
+      
+  def check_album(self,album): #Check for album
+    if self.albums.has_key(album):
+      return True
+    else:
+      return False
+      
+  def create_album(self,album): #New album
+    if self.check_album(album):
+      return False
+    else:
+      print("* New album " + album)
+      desc = raw_input("Add a description. Press q for exit: ")
+      if desc.capitalize() == "Q":
+        return False
+      else:
+        self.gclient.InsertAlbum(album, desc)
+        
+  def photoToName(self,photo):
+    pic = photo.split(".")
+    pic.pop()
+    newPic = ""
+    for a in pic:
+      newPic = newPic + "." + a
+    return newPic[1:]
+
+  def uploadPicture(self, album, photo, desc):
+    if self.check_album(album) == False:
+      if self.create_album(album) == False:
+        return False
+    album_url = '/data/feed/api/user/%s/albumid/%s' % (self.gclient.email, self.albums[album])
+    mime = mimetypes.guess_type(photo)[0]
+    photo = self.gclient.InsertPhotoSimple(album_url, self.photoToName(photo), desc, photo, mime)
+    
+def help:
+  print("Help\n")
+  print("Album list: List all your albums")
+  print("New Album: Add a new album")
+  print("Upload photo: Add a new photo")
+      
+             
 if __name__ == "__main__":
-  a = pypicasa()
-  a.login()
-  a.req_data()
+  googleobj = pypicasa()
+  googleobj.login()
+  googleobj.req_data()
+  while True:
+    a = raw_input("What are you doing?: ").capitalize()
+    if a == "Upload photo":
+      photo = raw_input("What filename?: ")
+      desc = raw_input("Photo description?: ")
+      album = raw_input("What album?: ")
+      if os.path.exists(photo):
+        googleobj.uploadPicture(album, photo, desc)
+      else:
+        print("Photo not found")
+      
+    if a == "Upload folder":
+      print("Not implemented")
+    if a == "New album":
+      album = raw_input("Whats your album name?: ")
+      b = googleobj.create_album(album)
+      if b == False:
+        print("Sorry, already exists")
+        
+    if a == "Album list":
+      print(googleobj.albums.keys())
+      
+    if a == "Q":
+      sys.exit(0)
