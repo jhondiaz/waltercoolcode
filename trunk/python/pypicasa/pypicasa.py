9e557fe606a56415ffd87ce1a91f4fa7d89aed9f
mrwaltercool <mrwaltercool@96f54e04-8c51-0410-8021-111a85c9a89a>
2009-10-31 19:18:22 +0000

    Pypicasa: - Folder upload completed! - Now im checking for is_folder? and is_supported?

--- trunk/python/pypicasa/pypicasa.py
+++ trunk/python/pypicasa/pypicasa.py
@@ -9,11 +9,21 @@
 
 import gdata
 import gdata.photos.service
-import sys, getpass, mimetypes, os
+import sys, getpass, mimetypes, os, dircache
 
 class pypicasa():
+  suppfiles = ["image/jpeg","image/png"]
   def __init__(self):
     self.gclient = gdata.photos.service.PhotosService()
+    
+  def is_supported(self, photo):
+    if os.path.isdir(photo) == True: #Avoid directories
+      return False
+    imptype = mimetypes.guess_type(photo)[0]
+    if self.suppfiles.count(imptype) == 0:
+      return False
+    return imptype
+    
   def login(self, email=None, passw=None):
     if email == None and passw == None:
       email = raw_input("Username: ")
@@ -34,6 +44,7 @@ class pypicasa():
     info = self.gclient.GetUserFeed()
     for x in info.entry:
       self.albums[x.title.text] = x.gphoto_id.text
+    return True
       
   def check_album(self,album): #Check for album
     if self.albums.has_key(album):
@@ -46,11 +57,13 @@ class pypicasa():
       return False
     else:
       print("* New album " + album)
-      desc = raw_input("Add a description. Press q for exit: ")
+      desc = raw_input("Add album description. Press q for exit: ")
       if desc.capitalize() == "Q":
         return False
       else:
         self.gclient.InsertAlbum(album, desc)
+        self.req_data()
+      return True
         
   def photoToName(self,photo):
     pic = photo.split(".")
@@ -65,14 +78,23 @@ class pypicasa():
       if self.create_album(album) == False:
         return False
     album_url = '/data/feed/api/user/%s/albumid/%s' % (self.gclient.email, self.albums[album])
-    mime = mimetypes.guess_type(photo)[0]
-    photo = self.gclient.InsertPhotoSimple(album_url, self.photoToName(photo), desc, photo, mime)
+    mime = self.is_supported(photo)
+    if mime == False:
+      print("Non photo or not supported")
+      return False
+    print("*Uploading " + photo)
+    try:
+      photo = self.gclient.InsertPhotoSimple(album_url, self.photoToName(photo), desc, photo, mime)
+    except socket.gaierror:
+      print("Error... retry...")
+      self.uploadPicture(album, photo, desc)
     
-def help:
-  print("Help\n")
-  print("Album list: List all your albums")
-  print("New Album: Add a new album")
-  print("Upload photo: Add a new photo")
+def help():
+  print("\nHelp")
+  print("*-Album list: List all your albums")
+  print("*-New Album: Add a new album")
+  print("*-Upload photo: Add a new photo")
+  print("*-Upload folder: Add all your photos on current folder")
       
              
 if __name__ == "__main__":
@@ -88,10 +110,14 @@ if __name__ == "__main__":
       if os.path.exists(photo):
         googleobj.uploadPicture(album, photo, desc)
       else:
-        print("Photo not found")
+        print("Photo not found.\n")
       
     if a == "Upload folder":
-      print("Not implemented")
+      album = raw_input("What album?: ")
+      for a in dircache.listdir("."):
+        googleobj.uploadPicture(album, a, "")
+      print("Folder images uploaded.\n")
+      
     if a == "New album":
       album = raw_input("Whats your album name?: ")
       b = googleobj.create_album(album)
@@ -100,6 +126,9 @@ if __name__ == "__main__":
         
     if a == "Album list":
       print(googleobj.albums.keys())
+      
+    if a == "Help":
+      help()
       
     if a == "Q":
       sys.exit(0)
